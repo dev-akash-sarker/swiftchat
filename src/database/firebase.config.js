70bf066410e9333db594996a87993f8220394e76
dev-akash-sarker <akashsarker210@gmail.com>
2023-10-16 13:53:34 +0600

    tailwindcss firebase added

---
+++ src/database/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVEli2shWzJXm68ePgc952qnjv10BvDlc",
+  authDomain: "swiftchat-817a3.firebaseapp.com",
+  projectId: "swiftchat-817a3",
+  storageBucket: "swiftchat-817a3.appspot.com",
+  messagingSenderId: "796165737270",
+  appId: "1:796165737270:web:1ec3eb2fd63e90541bc197",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
