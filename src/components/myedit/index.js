fae693fff48eb248b4b41b8e67052d2d930a46e1
dev-akash-sarker <akashsarker210@gmail.com>
2023-10-31 01:54:29 +0600

    update

--- src/components/myedit/index.js
+++ src/components/myedit/index.js
@@ -1,39 +1,85 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { GoUnverified, GoVerified } from "react-icons/go";
 import { RxReload } from "react-icons/rx";
 import { getData } from "country-list";
-import { getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
+
+import {
+  getAuth,
+  sendEmailVerification,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { ToastContainer, toast } from "react-toastify";
 import { useFormik } from "formik";
 import { getDatabase, ref, update } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+import { loginUsers } from "../../feature/slice/loginSlice";
+// import { getCities } from "countries-cities";
+// import { City } from "country-state-city";
+// import { cities } from "cities-list";
+// import { loginUsers } from "../../feature/slice/loginSlice";
 
 // import { Country, State, City } from "country-state-city";
 
 export default function MyEdit() {
-  const user = useSelector((state) => state.login.loggedIn);
+  // const cc = "US";
+  // const citiesInCountry = Object.values(cities[cc]);
+  // console.log(cities["london"]);
   const auth = getAuth();
-  const db = getDatabase();
+  const [islogout, setIslogout] = useState(false);
+  // const [myuser, setMyuser] = useState(getAuth().currentUser);
+  const user = useSelector((state) => state.login.loggedIn);
 
+  // useEffect(() => {
+  //   const unsubscribe = getAuth().onAuthStateChanged((user) => {
+  //     if (myuser) {
+  //       // If the user is authenticated, update the user state
+  //       setMyuser(myuser);
+  //     } else {
+  //       // If the user is not authenticated, set the user state to null
+  //       setMyuser(null);
+  //     }
+  //   });
+
+  //   // Clean up the listener when the component unmounts
+  //   return () => unsubscribe();
+  // }, [auth, myuser]);
+  const db = getDatabase();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const mineuser = auth.currentUser;
   const allcountry = getData();
+
   const formik = useFormik({
     initialValues: {
       firstname: user.displayName.split(" ")[0],
       lastname: user.displayName.split(" ")[1],
     },
+
     onSubmit: (e) => {
-      console.log("this is form", e);
-      console.log("f", e.firstname);
       updateProfile(mineuser, {
         displayName: e.firstname + " " + e.lastname,
       })
         .then(() => {
           update(ref(db, `${"users/" + user.uid}`), {
             username: e.firstname + " " + e.lastname,
-          });
-          // Profile updated!
-          // ...
+          })
+            .then(() => setIslogout(true))
+            .catch((error) => console.log(error));
+          toast.success(
+            "You will be automaticaly logout for changes! please login",
+            {
+              position: "top-right",
+              autoClose: 1500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            }
+          );
         })
         .catch((error) => {
           // An error occurred
@@ -59,9 +105,25 @@ export default function MyEdit() {
     });
   };
 
+  if (islogout === true) {
+    setTimeout(() => {
+      signOut(auth)
+        .then(() => {
+          localStorage.removeItem("users");
+          dispatch(loginUsers(null));
+          navigate("/signin");
+        })
+        .catch((error) => {
+          console.log(error.code);
+          console.log(error.message);
+        });
+    }, 1800);
+  }
+
   return (
     <>
       <ToastContainer />
+
       <div className="mt-12 ml-3 mr-2 text-[#2563eb] text-[12px] font-bold border-b-2 border-[#2563eb] inline-block">
         Edit Profile
       </div>
@@ -165,12 +227,12 @@ export default function MyEdit() {
                   </select>
                 </label>
                 <label className="flex flex-col gap-2 text-[#475569] text-[14px] font-[500]">
-                  Country
+                  State
                   <select
                     id="countries"
                     class="bg-gray-50 border border-gray-300 h-[39.6px] outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
-                    <option selected>Choose a country</option>
+                    <option selected>Choose a state</option>
                     {allcountry.map((item, i) => (
                       <>
                         <option value={item.code} key={i}>
